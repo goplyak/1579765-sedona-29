4ea092c4d7f144c160e4f102f7f8f4d07a265d92
goplyak <serega602@gmail.com>
2020-11-05 18:08:04 +0200

    правка после проверки

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,44 @@
 const searchBtn = document.querySelector(".search-button"),
-      searchBox = document.querySelector(".form-relative__item");
-searchBox.classList.add('search-hide-form');
+      searchBox = document.querySelector(".form-relative__item"),
+      searchForm = document.querySelector(".search-form__form"),
+      inputIn = document.querySelector("#date-in"),
+      inputOut = document.querySelector("#date-out");
+
+//add class hide to form
+searchBox.classList.add("search-hide-form");
+
+// open or close form
 searchBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
   searchBox.classList.toggle("search-hide-form");
+});
+
+// validate form
+searchForm.addEventListener("submit", function (evt) {
+  if(!inputIn.value || !inputOut.value){
+    evt.preventDefault();
+    searchBox.classList.add("error-form");
+    if(!inputIn.value){
+      inputIn.classList.add("error-item");
+    } else {
+      inputIn.classList.remove("error-item");
+    }
+    if(!inputOut.value){
+      inputOut.classList.add("error-item");
+    } else {
+      inputOut.classList.remove("error-item");
+    }
+  } else {
+    if (inputOut.classList.contains("error-item") ){
+      inputOut.classList.remove("error-item");
+    }
+    if (inputIn.classList.contains("error-item") ){
+      inputIn.classList.remove("error-item");
+    }
+  }
+  setTimeout(function () {
+    if (searchBox.classList.contains("error-form") ){
+      searchBox.classList.remove("error-form");
+    }
+  },1000);
 });
