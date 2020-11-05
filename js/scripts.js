88c91dc16946d8f7967d910a0c97167b219ff2fa
goplyak <serega602@gmail.com>
2020-11-05 18:14:25 +0200

    еще правка

--- js/scripts.js
+++ js/scripts.js
@@ -18,23 +18,6 @@ searchForm.addEventListener("submit", function (evt) {
   if(!inputIn.value || !inputOut.value){
     evt.preventDefault();
     searchBox.classList.add("error-form");
-    if(!inputIn.value){
-      inputIn.classList.add("error-item");
-    } else {
-      inputIn.classList.remove("error-item");
-    }
-    if(!inputOut.value){
-      inputOut.classList.add("error-item");
-    } else {
-      inputOut.classList.remove("error-item");
-    }
-  } else {
-    if (inputOut.classList.contains("error-item") ){
-      inputOut.classList.remove("error-item");
-    }
-    if (inputIn.classList.contains("error-item") ){
-      inputIn.classList.remove("error-item");
-    }
   }
   setTimeout(function () {
     if (searchBox.classList.contains("error-form") ){
